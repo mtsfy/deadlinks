92b15a71aa1998f1fb25c9da448886a0d2ef326c
Muse Tesfaye <tesfaye.musee@gmail.com>
2025-07-04 17:35:23 -0400

    feat(scraper): fetch the html from url

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,1 +1,20 @@
-package internal
+package scraper
+
+import (
+	"io"
+	"net/http"
+)
+
+func Fetch(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
